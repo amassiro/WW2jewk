c24c0219727f57383ddcc57bc26b1fe7a7b93ac9
Andrea <massironi.andrea@gmail.com>
2013-12-19 15:36:57 +0100

    -

--- Interference/README.md
+++ Interference/README.md
@@ -2,3 +2,18 @@ Interference WW+2jets ewk/qcd
 =======
 
 Interference between WW+2jets: ewk, qcd, ewk+qcd
+
+
+Working here:
+
+    cmsneu
+    /home/amassiro/Generation/MINLO/WW2jewk/Interference/
+
+LHE files here:
+
+    /data/amassiro/CMSSWLHE/WW2jetsPhantom/
+
+Dump sensitive information into a root file
+
+    c++ -o ntupleMaker.exe `root-config --glibs --cflags` `lhapdf-config --cppflags  --ldflags` -lm ntupleMaker.cpp
+    ./ntupleMaker.exe   blabla.lhe    blabla.root
---
+++ Interference/ntupleMaker.cpp
@@ -0,0 +1,193 @@
+// to compile:
+//   c++ -o ntupleMaker.exe `root-config --glibs --cflags` `lhapdf-config --cppflags  --ldflags` -lm ntupleMaker.cpp
+//
+// to use:
+//   ./ntupleMaker.exe fileLHE    outputFileRoot
+//
+
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <assert.h>
+
+
+#include "LHEF.h"
+
+
+#include "LHAPDF/LHAPDF.h"
+
+
+
+#include "TNtuple.h"
+#include "TFile.h"
+#include "TLorentzVector.h"
+#include "Math/Vector3D.h"
+#include "Math/Vector4D.h"
+
+
+struct ptsort: public std::binary_function<TLorentzVector, TLorentzVector, bool> {
+ bool operator () (const TLorentzVector & x, const TLorentzVector & y) {
+  return  (x.Perp () < y.Perp () ) ;
+ }
+} ;
+
+TLorentzVector buildP (const LHEF::HEPEUP & event, int iPart) {
+ TLorentzVector dummy ;
+ dummy.SetPxPyPzE (
+   event.PUP.at (iPart).at (0), // px
+   event.PUP.at (iPart).at (1), // py
+   event.PUP.at (iPart).at (2), // pz
+   event.PUP.at (iPart).at (3) // E
+   ) ;
+ return dummy ;
+}
+
+
+
+
+void fillNtuple (std::string fileNameLHE,  TNtuple & ntuple) {
+ std::ifstream ifs (fileNameLHE.c_str ()) ;
+ LHEF::Reader reader (ifs) ;
+
+ long ieve = 0 ;
+ long selected = 0 ;
+ double jetsNum = 0. ;
+
+ std::cout << " reading " << fileNameLHE << std::endl;
+
+ // loop over events
+ while ( reader.readEvent () ) {
+  ieve++;
+  if (ieve % 10000 == 0) std::cout << "event " << ieve << "\n" ;
+
+  TLorentzVector Higgs;
+  int iPartHiggs = -1;
+  float mH = 0;
+
+  std::vector<int> finalJets ;
+  std::vector<TLorentzVector> v_f_quarks ;
+  std::vector<TLorentzVector> v_f_leptons ;
+  std::vector<TLorentzVector> v_f_neutrinos ;
+
+  // loop over particles in the event
+  // and fill the variables of leptons and quarks
+  for (int iPart = 0 ; iPart < reader.hepeup.IDUP.size (); ++iPart) {
+
+   // look for the Higgs
+   if (abs (reader.hepeup.IDUP.at (iPart)) == 25) {
+    iPartHiggs = iPart;
+    Higgs.SetPxPyPzE  (
+      reader.hepeup.PUP.at (iPart).at (0), // px
+      reader.hepeup.PUP.at (iPart).at (1), // py
+      reader.hepeup.PUP.at (iPart).at (2), // pz
+      reader.hepeup.PUP.at (iPart).at (3) // E
+      );
+    mH = Higgs.M();
+   }
+
+   // outgoing particles
+   if (reader.hepeup.ISTUP.at (iPart) == 1) {
+    // quarks
+    if (abs (reader.hepeup.IDUP.at (iPart)) < 7) {
+     finalJets.push_back (iPart) ;
+     TLorentzVector dummy (
+       reader.hepeup.PUP.at (iPart).at (0), // px
+       reader.hepeup.PUP.at (iPart).at (1), // py
+       reader.hepeup.PUP.at (iPart).at (2), // pz
+       reader.hepeup.PUP.at (iPart).at (3) // E
+       ) ;
+     v_f_quarks.push_back (dummy) ;
+    } // quarks
+    else if (abs (reader.hepeup.IDUP.at (iPart)) == 11 || abs (reader.hepeup.IDUP.at (iPart)) == 13 || abs (reader.hepeup.IDUP.at (iPart)) == 15) {  // e = 11,   mu = 13,   tau = 15
+     TLorentzVector dummy (
+       reader.hepeup.PUP.at (iPart).at (0), // px
+       reader.hepeup.PUP.at (iPart).at (1), // py
+       reader.hepeup.PUP.at (iPart).at (2), // pz
+       reader.hepeup.PUP.at (iPart).at (3) // E
+       ) ;
+     v_f_leptons.push_back (dummy) ;
+    }
+    else if (abs (reader.hepeup.IDUP.at (iPart)) == 12 || abs (reader.hepeup.IDUP.at (iPart)) == 14 || abs (reader.hepeup.IDUP.at (iPart)) == 16) { // ve = 12,   vmu = 14,   vtau = 16
+     TLorentzVector dummy
+       (
+       reader.hepeup.PUP.at (iPart).at (0), // px
+       reader.hepeup.PUP.at (iPart).at (1), // py
+       reader.hepeup.PUP.at (iPart).at (2), // pz
+       reader.hepeup.PUP.at (iPart).at (3) // E
+       ) ;
+     v_f_neutrinos.push_back (dummy) ;
+    }
+   } // outgoing particles
+  } // loop over particles in the event
+
+  if (v_f_leptons.size () != 2) {
+   std::cout << " what !?!?!?! Not 2 leptons? Are you kidding?" << std::endl;
+   continue;
+  }
+
+
+  // sorting in pt
+  sort (v_f_quarks.rbegin (), v_f_quarks.rend (), ptsort ()) ;
+  sort (v_f_leptons.rbegin (), v_f_leptons.rend (), ptsort ()) ;
+
+  TLorentzVector diLepton = v_f_leptons.at (0) + v_f_leptons.at (1) ;
+  TLorentzVector missingEnergy = v_f_neutrinos.at (0) + v_f_neutrinos.at (1) ;
+
+  TLorentzVector dilepton_plus_dineutrinos = v_f_leptons.at (0) + v_f_leptons.at (1) + v_f_neutrinos.at (0) + v_f_neutrinos.at (1) ;
+
+  // the sum pf the two quarks
+
+  float jetpt1 = -99;
+  if (v_f_quarks.size()>0) jetpt1 = v_f_quarks.at (0).Pt ();
+  float jetpt2 = -99;
+  if (v_f_quarks.size()>1) jetpt2 = v_f_quarks.at (1).Pt ();
+
+  ntuple.Fill (
+    jetpt1,
+    jetpt2,
+    v_f_leptons.at (0).Pt (),
+    v_f_leptons.at (1).Pt (),
+    dilepton_plus_dineutrinos.M(),
+    diLepton.M()
+    ) ;
+
+ } // loop over events
+
+}
+
+
+
+int main (int argc, char **argv) { 
+ // Open a stream connected to an event file:
+ if (argc < 3) exit (1) ;
+
+ std::cout << " Input  LHE  =" << argv[1] << std::endl;
+ std::cout << " Output ROOT =" << argv[2] << std::endl;
+
+ 
+ const int SUBSET = 0 ;
+ const std::string NAME = "cteq6ll" ; //"cteq6l1"
+
+ LHAPDF::initPDFSet (NAME, LHAPDF::LHPDF, SUBSET) ;
+ const int NUMBER = LHAPDF::numberPDF () ;
+
+ LHAPDF::initPDF (0) ;
+
+
+ TNtuple ntu ("ntu", "ntu", "jetpt1:jetpt2:pt1:pt2:mWW:mll");
+ fillNtuple (argv[1], ntu) ;
+
+ TFile output (argv[2], "recreate") ;
+ output.cd() ;
+ ntu.Write();
+ output.Close();
+
+}
+
+
+
