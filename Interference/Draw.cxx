559ec1b702e8944b02ea1d0e9fd8ad9d7ff5889f
amassiro <massironi.andrea@gmail.com>
2013-12-24 16:28:51 +0100

    plot

---
+++ Interference/Draw.cxx
@@ -0,0 +1,130 @@
+void Draw(int kind = 0, std::string var = "mWW", int NBIN = 1000, int MIN = 0, int MAX = 1000) {
+ 
+ 
+ TString name1;
+ if (kind == 0) name1 = Form ("EW_jjme.root");
+ else name1 = Form ("EW_jjmm.root");
+ 
+ TString name2;
+ if (kind == 0) name2 = Form ("QCD_jjme.root");
+ else name2 = Form ("QCD_jjmm.root");
+ 
+ TString name3;
+ if (kind == 0) name3 = Form ("EWQCD_jjme.root");
+ else name3 = Form ("EWQCD_jjmm.root");
+ 
+ TFile* f1 = new TFile (name1.Data(),"READ"); // ---- EW
+ TFile* f2 = new TFile (name2.Data(),"READ"); // ---- QCD
+ TFile* f3 = new TFile (name3.Data(),"READ"); // ---- EW+QCD
+ 
+ std::cout << " name EW     = " << name1.Data() << std::endl;
+ std::cout << " name QCD    = " << name2.Data() << std::endl;
+ std::cout << " name EW+QCD = " << name3.Data() << std::endl;
+ 
+ TNtuple* t1 = (TNtuple*) f1->Get ("ntu");
+ TNtuple* t2 = (TNtuple*) f2->Get ("ntu");
+ TNtuple* t3 = (TNtuple*) f3->Get ("ntu");
+ 
+ TH1F* h_mWW_1 = new TH1F("h_mWW_1","h_mWW_1",NBIN,0,MAX);
+ TH1F* h_mWW_2 = new TH1F("h_mWW_2","h_mWW_2",NBIN,0,MAX);
+ TH1F* h_mWW_3 = new TH1F("h_mWW_3","h_mWW_3",NBIN,0,MAX);
+ TH1F* h_Ratio = new TH1F("h_mWW_ratio","h_mWW_ratio",NBIN,0,MAX);
+ TH1F* h_I = new TH1F("h_I","h_I",NBIN,0,MAX);
+ 
+ //---- me
+ if (kind == 0) gROOT->ProcessLine ("float xsecEW[100] = {2.68719361245196228E-002};");
+ //---- mm
+ if (kind == 1) gROOT->ProcessLine ("float xsecEW[100] = {1.35241401169139651E-002};");
+ 
+ //---- me
+ if (kind == 0) gROOT->ProcessLine ("float xsecQCD[100] = {0.62100954917703377};");
+ //---- mm
+ if (kind == 1) gROOT->ProcessLine ("float xsecQCD[100] = {0.31107029810623943};");
+ 
+ //---- me
+ if (kind == 0) gROOT->ProcessLine ("float xsecEWQCD[100] = {0.64303443036305363};");
+ //---- mm
+ if (kind == 1) gROOT->ProcessLine ("float xsecEWQCD[100] = {0.32257867984679178};");
+
+ 
+//  TString cut = Form ("jetpt1>30 && jetpt2>30 && mWW>130");
+//  TString cut = Form ("jetpt1>30 && jetpt2>30");
+ TString cut = Form ("1");
+ 
+ int tot_1 = t1 -> GetEntries();
+ int tot_2 = t2 -> GetEntries();
+ int tot_3 = t3 -> GetEntries();
+ 
+ std::cout << std::endl;
+ std::cout << " tot_1 = " << tot_1 << std::endl;
+ std::cout << " tot_2 = " << tot_2 << std::endl;
+ std::cout << " tot_3 = " << tot_3 << std::endl;
+ 
+ TString weightWithXsecQCD   = Form ("(%s) * (%f) * (%f)",cut.Data(),xsecQCD[0],1000./tot_1);
+ TString weightWithXsecEW    = Form ("(%s) * (%f) * (%f)",cut.Data(),xsecEW[0],1000./tot_2);
+ TString weightWithXsecEWQCD = Form ("(%s) * (%f) * (%f)",cut.Data(),xsecEWQCD[0],1000./tot_3);
+ 
+ TString toDraw;
+ toDraw = Form ("%s >> h_mWW_1",var.c_str());
+ t1->Draw(toDraw.Data(),weightWithXsecQCD.Data(),"goff");
+ toDraw = Form ("%s >> h_mWW_2",var.c_str());
+ t2->Draw(toDraw.Data(),weightWithXsecEW.Data(), "goff");
+ toDraw = Form ("%s >> h_mWW_3",var.c_str());
+ t3->Draw(toDraw.Data(),weightWithXsecEWQCD.Data(), "goff");
+ 
+ h_mWW_1->SetLineColor(kBlue);
+ h_mWW_2->SetLineColor(kRed);
+ h_mWW_3->SetLineColor(kGreen);
+ 
+ h_mWW_1->SetLineStyle(1);
+ h_mWW_2->SetLineStyle(2);
+ h_mWW_3->SetLineStyle(2);
+ 
+ h_mWW_1->SetLineWidth(2);
+ h_mWW_2->SetLineWidth(2);
+ h_mWW_3->SetLineWidth(4);
+ 
+ for (int iBin = 0; iBin < h_mWW_1->GetNbinsX(); iBin++) {
+  float ewk = h_mWW_1->GetBinContent(iBin+1);
+  float qcd = h_mWW_2->GetBinContent(iBin+1);  
+  float ewkqcd = h_mWW_3->GetBinContent(iBin+1);
+  h_I -> SetBinContent (iBin+1, ewkqcd - ewk - qcd);
+  if (ewk != 0) h_Ratio -> SetBinContent (iBin+1, (ewkqcd - qcd) / ewk);
+  else h_Ratio -> SetBinContent (iBin+1, 0);
+ }
+ 
+ 
+ 
+ TCanvas* cc_All = new TCanvas("cc_All","cc_All",800,600);
+ h_mWW_3->GetYaxis()->SetRangeUser(0.01,h_mWW_3->GetMaximum()*1.5);
+ h_mWW_3 -> Draw();
+ h_mWW_1 -> Draw("same");
+ h_mWW_2 -> Draw("same");
+ cc_All->SetGrid();
+ 
+ 
+ TCanvas* cc_I = new TCanvas("cc_I","cc_I",800,600);
+ h_I->GetYaxis()->SetRangeUser(h_I->GetMinimum()-10.,h_I->GetMaximum()*2.5);
+ h_I->SetLineColor(kMagenta);
+ h_I->SetLineStyle(1);
+ h_I->SetLineWidth(2);
+ h_I -> Draw();
+ cc_I->SetGrid();
+ 
+ 
+ TCanvas* cc_Ratio = new TCanvas("cc_Ratio","cc_Ratio",800,600);
+ h_Ratio->SetLineColor(kGreen);
+ h_Ratio->SetLineStyle(1);
+ h_Ratio->SetLineWidth(2);
+ h_Ratio -> Draw();
+ cc_Ratio->SetGrid();
+ 
+ std::cout << std::endl;
+ std::cout << " --------------------------- " << std::endl;
+ std::cout << " ewk     = " << h_mWW_1->Integral() << std::endl;
+ std::cout << " qcd     = " << h_mWW_2->Integral() << std::endl;
+ std::cout << " ewkqcd  = " << h_mWW_3->Integral() << std::endl;
+ std::cout << " ewk+qcd = " << h_mWW_1->Integral() + h_mWW_2->Integral() << std::endl;
+ std::cout << " --------------------------- " << std::endl;
+ std::cout << std::endl; 
+}
