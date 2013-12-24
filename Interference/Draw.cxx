b0ba00b7f3521c9d3b0ce093fae617a67dcb01ad
amassiro <massironi.andrea@gmail.com>
2013-12-24 16:51:22 +0100

    plot

--- Interference/Draw.cxx
+++ Interference/Draw.cxx
@@ -29,6 +29,7 @@ void Draw(int kind = 0, std::string var = "mWW", int NBIN = 1000, int MIN = 0, i
  TH1F* h_mWW_2 = new TH1F("h_mWW_2","h_mWW_2",NBIN,0,MAX);
  TH1F* h_mWW_3 = new TH1F("h_mWW_3","h_mWW_3",NBIN,0,MAX);
  TH1F* h_Ratio = new TH1F("h_mWW_ratio","h_mWW_ratio",NBIN,0,MAX);
+ TH1F* h_Subtraction = new TH1F("h_Subtraction","h_Subtraction",NBIN,0,MAX);
  TH1F* h_I = new TH1F("h_I","h_I",NBIN,0,MAX);
  
  //---- me
@@ -60,21 +61,23 @@ void Draw(int kind = 0, std::string var = "mWW", int NBIN = 1000, int MIN = 0, i
  std::cout << " tot_2 = " << tot_2 << std::endl;
  std::cout << " tot_3 = " << tot_3 << std::endl;
  
- TString weightWithXsecQCD   = Form ("(%s) * (%f) * (%f)",cut.Data(),xsecQCD[0],1000./tot_1);
  TString weightWithXsecEW    = Form ("(%s) * (%f) * (%f)",cut.Data(),xsecEW[0],1000./tot_2);
+ TString weightWithXsecQCD   = Form ("(%s) * (%f) * (%f)",cut.Data(),xsecQCD[0],1000./tot_1);
  TString weightWithXsecEWQCD = Form ("(%s) * (%f) * (%f)",cut.Data(),xsecEWQCD[0],1000./tot_3);
  
  TString toDraw;
  toDraw = Form ("%s >> h_mWW_1",var.c_str());
- t1->Draw(toDraw.Data(),weightWithXsecQCD.Data(),"goff");
+ t1->Draw(toDraw.Data(),weightWithXsecEW.Data(),"goff");
+ 
  toDraw = Form ("%s >> h_mWW_2",var.c_str());
- t2->Draw(toDraw.Data(),weightWithXsecEW.Data(), "goff");
+ t2->Draw(toDraw.Data(),weightWithXsecQCD.Data(), "goff");
+ 
  toDraw = Form ("%s >> h_mWW_3",var.c_str());
  t3->Draw(toDraw.Data(),weightWithXsecEWQCD.Data(), "goff");
  
- h_mWW_1->SetLineColor(kBlue);
- h_mWW_2->SetLineColor(kRed);
- h_mWW_3->SetLineColor(kGreen);
+ h_mWW_1->SetLineColor(kBlue);  //---- ewk
+ h_mWW_2->SetLineColor(kRed);   //---- qcd
+ h_mWW_3->SetLineColor(kGreen); //---- ewk+qcd
  
  h_mWW_1->SetLineStyle(1);
  h_mWW_2->SetLineStyle(2);
@@ -84,6 +87,19 @@ void Draw(int kind = 0, std::string var = "mWW", int NBIN = 1000, int MIN = 0, i
  h_mWW_2->SetLineWidth(2);
  h_mWW_3->SetLineWidth(4);
  
+ TLegend* leg = new TLegend(0.5,0.7,0.9,0.9);
+ leg->AddEntry(h_mWW_1,"ewk","l");
+ leg->AddEntry(h_mWW_2,"qcd","l");
+ leg->AddEntry(h_mWW_3,"ewk+qcd","l");
+ leg->SetFillStyle(0);
+
+
+ TLegend* leg2 = new TLegend(0.5,0.7,0.9,0.9);
+ leg2->AddEntry(h_mWW_1,"ewk","l");
+ leg2->AddEntry(h_Subtraction,"ewk+interference","l");
+ leg2->SetFillStyle(0);
+
+ 
  for (int iBin = 0; iBin < h_mWW_1->GetNbinsX(); iBin++) {
   float ewk = h_mWW_1->GetBinContent(iBin+1);
   float qcd = h_mWW_2->GetBinContent(iBin+1);  
@@ -91,15 +107,17 @@ void Draw(int kind = 0, std::string var = "mWW", int NBIN = 1000, int MIN = 0, i
   h_I -> SetBinContent (iBin+1, ewkqcd - ewk - qcd);
   if (ewk != 0) h_Ratio -> SetBinContent (iBin+1, (ewkqcd - qcd) / ewk);
   else h_Ratio -> SetBinContent (iBin+1, 0);
+  h_Subtraction -> SetBinContent (iBin+1, ewkqcd - qcd);
  }
  
  
  
  TCanvas* cc_All = new TCanvas("cc_All","cc_All",800,600);
  h_mWW_3->GetYaxis()->SetRangeUser(0.01,h_mWW_3->GetMaximum()*1.5);
- h_mWW_3 -> Draw();
- h_mWW_1 -> Draw("same");
- h_mWW_2 -> Draw("same");
+ h_mWW_3 -> Draw();  //---- ewk+qcd
+ h_mWW_1 -> Draw("same"); //---- ewk
+ h_mWW_2 -> Draw("same"); //---- qcd
+ leg->Draw();
  cc_All->SetGrid();
  
  
@@ -111,6 +129,17 @@ void Draw(int kind = 0, std::string var = "mWW", int NBIN = 1000, int MIN = 0, i
  h_I -> Draw();
  cc_I->SetGrid();
  
+ 
+ TCanvas* cc_Sub = new TCanvas("cc_Sub","cc_Sub",800,600);
+ h_Subtraction->GetYaxis()->SetRangeUser(h_Subtraction->GetMinimum()-10.,h_Subtraction->GetMaximum()*2.5);
+ h_Subtraction->SetLineColor(kMagenta);
+ h_Subtraction->SetLineStyle(2);
+ h_Subtraction->SetLineWidth(3);
+ h_Subtraction -> Draw();
+ h_mWW_1 -> Draw("same");
+ leg2->Draw();
+ cc_Sub->SetGrid();
+
  
  TCanvas* cc_Ratio = new TCanvas("cc_Ratio","cc_Ratio",800,600);
  h_Ratio->SetLineColor(kGreen);
