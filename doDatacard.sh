94f0e81119ea919ae08df71d8c970dfd38f8bbb9
amassiro <massironi.andrea@gmail.com>
2014-01-06 14:31:50 +0100

    add doDatacard.sh

--- doDatacard.sh
+++ doDatacard.sh
@@ -19,24 +19,28 @@ pack.py -p WW2jewkSFcutTCHE05
 
 cd WW2jewkDFshapeTCHE21
 bash doIt.sh
+mkAutopsy.py datacards/hww-19.36fb.mH125.of_2j_shape.txt
 cd ..
 rm WW2jewkDFshapeTCHE21.tgz
 pack.py -p WW2jewkDFshapeTCHE21
 
 cd WW2jewkDFshapeTCHE05
 bash doIt.sh
+mkAutopsy.py datacards/hww-19.36fb.mH125.of_2j_shape.txt
 cd ..
 rm WW2jewkDFshapeTCHE05.tgz
 pack.py -p WW2jewkDFshapeTCHE05
 
 cd WW2jewkDFcutTCHE05
 bash doIt.sh
+mkAutopsy.py datacards/hww-19.36fb.mH125.of_2j_shape.txt
 cd ..
 rm WW2jewkDFcutTCHE05.tgz
 pack.py -p WW2jewkDFcutTCHE05
 
 cd WW2jewkDFcutTCHE21
 bash doIt.sh
+mkAutopsy.py datacards/hww-19.36fb.mH125.of_2j_shape.txt
 cd ..
 rm WW2jewkDFcutTCHE21.tgz
 pack.py -p WW2jewkDFcutTCHE21
