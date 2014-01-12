c3c06d7e95309989e02d08655febcb38b785e4fd
amassiro <massironi.andrea@gmail.com>
2014-01-12 20:32:36 +0100

    last

---
+++ ttbar/dottbarshape.sh
@@ -0,0 +1,91 @@
+
+# ---- shape based analysis
+# GetTop_Macro_forDataCard_Complete(int iWP, std::string suffix = "of", std::string whatAnalysis = "shape", bool doEnrichToGetEvents = false, int binCutPt = 0, int binTCHE = 0
+
+# pt 0 thce 0
+
+rm test/WW2jewk/ttbar/result-eff-shape-of-pt-0-tche-0.txt
+root -l test/WW2jewk/ttbar/GetTop-Macro-forDataCard-Complete.cxx\(0,\"of\"\,\"shape\",true,0,0\)
+rm TopCard_2j-shape-of-pt-0-tche-0.txt
+cat test/WW2jewk/ttbar/result-shape-of-pt-0-tche-0-0.txt >>  TopCard_2j-shape-of-pt-0-tche-0.txt
+rm TopCard_2j-shape-of-pt-0-tche-0-ScaleFactor.txt
+cat test/WW2jewk/ttbar/result-SF-shape-of-pt-0-tche-0-0.txt >>  TopCard_2j-shape-of-pt-0-tche-0-ScaleFactor.txt
+
+
+# pt 0 thce 1
+
+rm test/WW2jewk/ttbar/result-eff-shape-of-pt-0-tche-1.txt
+root -l test/WW2jewk/ttbar/GetTop-Macro-forDataCard-Complete.cxx\(0,\"of\"\,\"shape\",true,0,1\)
+rm TopCard_2j-shape-of-pt-0-tche-1.txt
+cat test/WW2jewk/ttbar/result-shape-of-pt-0-tche-1-0.txt >>  TopCard_2j-shape-of-pt-0-tche-1.txt
+rm TopCard_2j-shape-of-pt-0-tche-1-ScaleFactor.txt
+cat test/WW2jewk/ttbar/result-SF-shape-of-pt-0-tche-1-0.txt >>  TopCard_2j-shape-of-pt-0-tche-1-ScaleFactor.txt
+
+
+# pt 1 thce 0
+
+rm test/WW2jewk/ttbar/result-eff-shape-of-pt-1-tche-0.txt
+root -l test/WW2jewk/ttbar/GetTop-Macro-forDataCard-Complete.cxx\(0,\"of\"\,\"shape\",true,1,0\)
+rm TopCard_2j-shape-of-pt-1-tche-0.txt
+cat test/WW2jewk/ttbar/result-shape-of-pt-1-tche-0-0.txt >>  TopCard_2j-shape-of-pt-1-tche-0.txt
+rm TopCard_2j-shape-of-pt-1-tche-0-ScaleFactor.txt
+cat test/WW2jewk/ttbar/result-SF-shape-of-pt-1-tche-0-0.txt >>  TopCard_2j-shape-of-pt-1-tche-0-ScaleFactor.txt
+
+
+# pt 1 thce 1
+
+rm test/WW2jewk/ttbar/result-eff-shape-of-pt-1-tche-1.txt
+root -l test/WW2jewk/ttbar/GetTop-Macro-forDataCard-Complete.cxx\(0,\"of\"\,\"shape\",true,1,1\)
+rm TopCard_2j-shape-of-pt-1-tche-1.txt
+cat test/WW2jewk/ttbar/result-shape-of-pt-1-tche-1-0.txt >>  TopCard_2j-shape-of-pt-1-tche-1.txt
+rm TopCard_2j-shape-of-pt-1-tche-1-ScaleFactor.txt
+cat test/WW2jewk/ttbar/result-SF-shape-of-pt-1-tche-1-0.txt >>  TopCard_2j-shape-of-pt-1-tche-1-ScaleFactor.txt
+
+
+# pt 2 thce 0
+
+rm test/WW2jewk/ttbar/result-eff-shape-of-pt-2-tche-0.txt
+root -l test/WW2jewk/ttbar/GetTop-Macro-forDataCard-Complete.cxx\(0,\"of\"\,\"shape\",true,2,0\)
+rm TopCard_2j-shape-of-pt-2-tche-0.txt
+cat test/WW2jewk/ttbar/result-shape-of-pt-2-tche-0-0.txt >>  TopCard_2j-shape-of-pt-2-tche-0.txt
+rm TopCard_2j-shape-of-pt-2-tche-0-ScaleFactor.txt
+cat test/WW2jewk/ttbar/result-SF-shape-of-pt-2-tche-0-0.txt >>  TopCard_2j-shape-of-pt-2-tche-0-ScaleFactor.txt
+
+
+pt 2 thce 1
+
+rm test/WW2jewk/ttbar/result-eff-shape-of-pt-2-tche-1.txt
+root -l test/WW2jewk/ttbar/GetTop-Macro-forDataCard-Complete.cxx\(0,\"of\"\,\"shape\",true,2,1\)
+rm TopCard_2j-shape-of-pt-2-tche-1.txt
+cat test/WW2jewk/ttbar/result-shape-of-pt-2-tche-1-0.txt >>  TopCard_2j-shape-of-pt-2-tche-1.txt
+rm TopCard_2j-shape-of-pt-2-tche-1-ScaleFactor.txt
+cat test/WW2jewk/ttbar/result-SF-shape-of-pt-2-tche-1-0.txt >>  TopCard_2j-shape-of-pt-2-tche-1-ScaleFactor.txt
+
+
+
+
+
+
+
+# pt 3 thce 0
+
+rm test/WW2jewk/ttbar/result-eff-shape-of-pt-3-tche-0.txt
+root -l test/WW2jewk/ttbar/GetTop-Macro-forDataCard-Complete.cxx\(0,\"of\"\,\"shape\",true,3,0\)
+rm TopCard_2j-shape-of-pt-3-tche-0.txt
+cat test/WW2jewk/ttbar/result-shape-of-pt-3-tche-0-0.txt >>  TopCard_2j-shape-of-pt-3-tche-0.txt
+rm TopCard_2j-shape-of-pt-3-tche-0-ScaleFactor.txt
+cat test/WW2jewk/ttbar/result-SF-shape-of-pt-3-tche-0-0.txt >>  TopCard_2j-shape-of-pt-3-tche-0-ScaleFactor.txt
+
+
+pt 3 thce 1
+
+rm test/WW2jewk/ttbar/result-eff-shape-of-pt-3-tche-1.txt
+root -l test/WW2jewk/ttbar/GetTop-Macro-forDataCard-Complete.cxx\(0,\"of\"\,\"shape\",true,3,1\)
+rm TopCard_2j-shape-of-pt-3-tche-1.txt
+cat test/WW2jewk/ttbar/result-shape-of-pt-3-tche-1-0.txt >>  TopCard_2j-shape-of-pt-3-tche-1.txt
+rm TopCard_2j-shape-of-pt-3-tche-1-ScaleFactor.txt
+cat test/WW2jewk/ttbar/result-SF-shape-of-pt-3-tche-1-0.txt >>  TopCard_2j-shape-of-pt-3-tche-1-ScaleFactor.txt
+
+
+
+
